3f265d67f0c0552339a712a008627478fe6bb4ef
siddarthnachannagari <mnachan@purdue.edu>
2024-11-18 19:32:58 -0500

    Fixed RNN graph

--- rsquare_graph.py
+++ rsquare_graph.py
@@ -5,11 +5,9 @@ r_square_val_dict = {
 
     "RB": 0.724,
 
-    "WR": 0.99,
-
     "TE": 0.83,
 
-    "OL": 0.99,
+    "OL": 0.87,
 
     "Defense": 0.82
 }
@@ -20,7 +18,6 @@ r_square_values = list(r_square_val_dict.values())
 colors = {
     "QB": "#5DADE2",  # Light Blue
     "RB": "#76D7C4",  # Mint Green
-    "WR": "#F7DC6F",  # Soft Yellow
     "TE": "#E74C3C",  # Neon Red
     "OL": "#AF7AC5",  # Purple
     "Defense": "#58D68D"  # Neon Green
@@ -34,7 +31,7 @@ bars = plt.bar(positions, r_square_values, color=bar_colors, edgecolor='black')
 # Add labels and title
 plt.xlabel('Position Groups', fontsize=12)
 plt.ylabel('R² Value', fontsize=12)
-plt.title('Player Performance R² Model Values by Position Group (2022)', fontsize=14)
+plt.title('Player Performance R² Model Values by Position Group Using RNN (2022)', fontsize=14)
 plt.ylim(0, 1.1)  # Set y-axis limit to show the full range of R² values
 
 plt.grid(axis='y', linestyle='--', alpha=0.7)
