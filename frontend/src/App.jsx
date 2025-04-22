83d3d263accf25ef2693b9d4563bbafcdbf66512
tandon35 <tandon35@purdue.edu>
2025-04-21 20:14:57 -0400

    Added team roster page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,32 +5,28 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Results from './pages/Results';
-import PlayerPage from './pages/PlayerPage'
+import PlayerPage from './pages/PlayerPage';
 import PlayerRanking from './pages/PlayerRanking';
+import TeamPffRanking from './pages/TeamPffRanking';
 
 import './App.css';
 
 function App() {
   return (
-    <GlobalProvider> {/* All components within this provider will be able to access GlobalContext via useContext. */}
+    <GlobalProvider>
+      <BrowserRouter>
+        <Navbar />
 
-      <BrowserRouter> {/* Within this, we can specify webapp routing. */}
-
-        {/* Navbar must go inside browser router so the <Link> component works. */}
-        <Navbar/>
-
-        {/* Route each path to the corresponding component. */}
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/results" element={<Results/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/results" element={<Results />} />
           <Route path="/player/:playerName" element={<PlayerPage />} />
           <Route path="/player-rankings" element={<PlayerRanking />} />
+          <Route path="/team-pff" element={<TeamPffRanking />} />
         </Routes>
-
       </BrowserRouter>
-
     </GlobalProvider>
   );
 }
--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -18,6 +18,7 @@ const Navbar = () => {
                 <Link to="/signup">Signup</Link>
                 <Link to="/results">Results</Link>
                 <Link to="/player-rankings">Player Rankings</Link>
+                <Link to="/team-pff">Team Rosters</Link>
             </div>
         </div>
     );
